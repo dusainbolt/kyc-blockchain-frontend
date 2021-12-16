f26ac3d293a92f3093d01e780c04d835de9dcd3d
Du Le <du.le@ekoios.vn>
2021-12-16 18:44:23 +0700

    new blockchain init project

---
+++ pages/sup.tsx
@@ -0,0 +1,37 @@
+import { FC } from 'react';
+import { Button } from '@material-ui/core';
+import { makeStyles, createStyles } from '@material-ui/core/styles';
+import styles from '../styles/Home.module.css';
+
+interface SupProps {
+  message: string;
+}
+
+const useStyles = makeStyles(() => ({
+  root: {
+    backgroundColor: 'red',
+    fontSize: 22,
+  },
+}));
+
+const Sup: FC<SupProps> = ({ message }: SupProps) => {
+  const classes = useStyles();
+  return (
+    <div className={styles.container}>
+      <button type="button">I am so fucking boring!</button>
+      <Button className={classes.root} variant="contained">
+        {message}
+      </Button>
+    </div>
+  );
+};
+
+export const getServerSideProps = () => {
+  return {
+    props: {
+      message: 'sup',
+    },
+  };
+};
+
+export default Sup;
