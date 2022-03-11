0b1431a5832d15f29afb1211026ff4a4fa76dfd8
le.huy.du <SUN-ASTERISK\le.huy.du@b122952-pc.sun-asterisk.com>
2022-03-11 23:27:24 +0700

    slide bar

--- src/common/Layout/index.tsx
+++ src/common/Layout/index.tsx
@@ -1,6 +1,20 @@
 import { layoutStyle } from './layoutStyle';
 import LogoDevIcon from '@mui/icons-material/LogoDev';
-import { Breadcrumbs, Divider, IconButton, InputBase, Link, ListItemIcon, Typography } from '@mui/material';
+import {
+  Backdrop,
+  Breadcrumbs,
+  CircularProgress,
+  Divider,
+  IconButton,
+  InputBase,
+  Link,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemIcon,
+  Typography,
+  ListItemText,
+} from '@mui/material';
 import { FC, ReactNode, useState } from 'react';
 import { BreadcrumbsType } from '@type/layout';
 import SearchIcon from '@mui/icons-material/Search';
@@ -11,6 +25,9 @@ import MenuItem from '@mui/material/MenuItem';
 import { MenuCustom } from '@common/Menu/MenuCustom';
 import { Logout } from '@mui/icons-material';
 import { useWeb3React } from '@web3-react/core';
+import { getAuthSlice } from '@redux/slices/authSlice';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import InboxIcon from '@mui/icons-material/Inbox';
 
 interface LayoutProps {
   children: ReactNode;
@@ -20,6 +37,7 @@ interface LayoutProps {
 export const Layout: FC<LayoutProps> = ({ children, breadcrumbs }) => {
   const classes = layoutStyle();
   const { address } = useAppSelector(getWalletSlice);
+  const { token } = useAppSelector(getAuthSlice);
   const [anchorEl, setAnchorEl] = useState(null);
   const { deactivate } = useWeb3React();
 
@@ -36,7 +54,9 @@ export const Layout: FC<LayoutProps> = ({ children, breadcrumbs }) => {
     deactivate();
   };
 
-  return (
+  const showContentAuth = token && address;
+
+  return showContentAuth ? (
     <>
       <div className={classes.header}>
         <div className={classes.logoWrap}>
@@ -62,7 +82,9 @@ export const Layout: FC<LayoutProps> = ({ children, breadcrumbs }) => {
           </IconButton>
         </div>
         <div onClick={handleMenu} className={classes.walletWrap}>
-          {Helper.splitString(address)}
+          <div className={classes.walletAddress}>
+            <AccountCircleIcon /> <span>{Helper.splitString(address)}</span>
+          </div>
         </div>
         <MenuCustom id="account-wallet-menu" open={Boolean(anchorEl)} handleClose={handleClose} anchorEl={anchorEl}>
           <MenuItem onClick={handleClose}>Profile</MenuItem>
@@ -75,7 +97,43 @@ export const Layout: FC<LayoutProps> = ({ children, breadcrumbs }) => {
           </MenuItem>
         </MenuCustom>
       </div>
-      {children}
+      <div className={classes.body}>
+        <div className={classes.sidebar}>
+          <div className={classes.sidebarWrap}>
+            <List style={{ width: '100%' }}>
+              <ListItem className={classes.navWrap} selected disablePadding>
+                <ListItemButton>
+                  <ListItemIcon>
+                    <InboxIcon />
+                  </ListItemIcon>
+                  <ListItemText primary="Profile" />
+                </ListItemButton>
+              </ListItem>
+              <ListItem className={classes.navWrap} disablePadding>
+                <ListItemButton>
+                  <ListItemIcon>
+                    <InboxIcon />
+                  </ListItemIcon>
+                  <ListItemText primary="History" />
+                </ListItemButton>
+              </ListItem>
+              <ListItem className={classes.navWrap} disablePadding>
+                <ListItemButton>
+                  <ListItemIcon>
+                    <InboxIcon />
+                  </ListItemIcon>
+                  <ListItemText primary="Share History" />
+                </ListItemButton>
+              </ListItem>
+            </List>
+          </div>
+        </div>
+        <div className={classes.bodyContent}>{children}</div>
+      </div>
     </>
+  ) : (
+    <Backdrop sx={{ color: '#fff', zIndex: (theme) => theme.zIndex.drawer + 1 }} open onClick={handleClose}>
+      <CircularProgress color="inherit" />
+    </Backdrop>
   );
 };
